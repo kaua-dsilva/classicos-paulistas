973358204c90ad01078bc1346d4be4a737409065
kaua-dsilva <kaua.dsilva@sptech.school>
2025-06-02 07:49:17 -0300

    atualização

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const path = require("path");
+const router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("index");
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
